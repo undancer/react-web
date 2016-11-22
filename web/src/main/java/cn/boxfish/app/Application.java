e9ecbabe8eb28f85f698f3f9f3000ec588b4d89c
undancer <undancer@gmail.com>
2016-11-22 11:28:00 +0800

    提交修改

---
+++ web/src/main/java/cn/boxfish/app/Application.java
@@ -0,0 +1,14 @@
+package cn.boxfish.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by undancer on 2016/11/22.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
